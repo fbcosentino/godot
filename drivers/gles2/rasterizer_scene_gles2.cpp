d97552e8113720bb43a404ed389d76f44f140f28
Omar El Sheikh <techmanozmar@gmail.com>
2021-03-08 12:58:13 -0500

    Octahedral Normal/Tangent Compression
    Implement Octahedral Compression for normal/tangent vectors
    *Oct32 for uncompressed vectors
    *Oct16 for compressed vectors
    
    Reduces vertex size for each attribute by
    *Uncompressed: 12 bytes, vec4<float32> -> vec2<unorm16>
    *Compressed: 2 bytes, vec4<unorm8> -> vec2<unorm8>
    
    Binormal sign is encoded in the y coordinate of the encoded tangent
    
    Added conversion functions to go from octahedral mapping to cartesian
    for normal and tangent vectors
    
    sprite_3d and soft_body meshes write to their vertex buffer memory
    directly and need to convert their normals and tangents to the new oct
    format before writing
    
    Created a new mesh flag to specify whether a mesh is using octahedral
    compression or not
    Updated documentation to discuss new flag/defaults
    
    Created shader flags to specify whether octahedral or cartesian vectors
    are being used
    
    Updated importers to use octahedral representation as the default format
    for importing meshes
    
    Updated ShaderGLES2 to support 64 bit version codes as we hit the limit
    of the 32-bit integer that was previously used as a bitset to store
    enabled/disabled flags

--- drivers/gles2/rasterizer_scene_gles2.cpp
+++ drivers/gles2/rasterizer_scene_gles2.cpp
@@ -2438,6 +2438,8 @@ void RasterizerSceneGLES2::_render_render_list(RenderList::Element **p_elements,
 	RasterizerStorageGLES2::Skeleton *prev_skeleton = nullptr;
 	RasterizerStorageGLES2::GeometryOwner *prev_owner = nullptr;
 
+	bool prev_octahedral_compression = false;
+
 	Transform view_transform_inverse = p_view_transform.inverse();
 	CameraMatrix projection_inverse = p_projection.inverse();
 
@@ -2687,6 +2689,12 @@ void RasterizerSceneGLES2::_render_render_list(RenderList::Element **p_elements,
 			storage->info.render.surface_switch_count++;
 		}
 
+		bool octahedral_compression = ((RasterizerStorageGLES2::Surface *)e->geometry)->format & VisualServer::ArrayFormat::ARRAY_FLAG_USE_OCTAHEDRAL_COMPRESSION;
+		if (octahedral_compression != prev_octahedral_compression) {
+			state.scene_shader.set_conditional(SceneShaderGLES2::ENABLE_OCTAHEDRAL_COMPRESSION, octahedral_compression);
+			rebind = true;
+		}
+
 		bool shader_rebind = false;
 		if (rebind || material != prev_material) {
 			storage->info.render.material_switch_count++;
@@ -2804,6 +2812,7 @@ void RasterizerSceneGLES2::_render_render_list(RenderList::Element **p_elements,
 		prev_material = material;
 		prev_skeleton = skeleton;
 		prev_instancing = instancing;
+		prev_octahedral_compression = octahedral_compression;
 		prev_light = light;
 		prev_refprobe_1 = refprobe_1;
 		prev_refprobe_2 = refprobe_2;
@@ -2812,6 +2821,7 @@ void RasterizerSceneGLES2::_render_render_list(RenderList::Element **p_elements,
 	}
 
 	_setup_light_type(nullptr, nullptr); //clear light stuff
+	state.scene_shader.set_conditional(SceneShaderGLES2::ENABLE_OCTAHEDRAL_COMPRESSION, false);
 	state.scene_shader.set_conditional(SceneShaderGLES2::USE_SKELETON, false);
 	state.scene_shader.set_conditional(SceneShaderGLES2::SHADELESS, false);
 	state.scene_shader.set_conditional(SceneShaderGLES2::BASE_PASS, false);
